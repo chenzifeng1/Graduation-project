7366adf589c90fda5182dd1986b92e7a0940c466
704734862 <chenzifeng527@gmail.com>
2019-05-11 16:52:13 +0800

    图书馆搜索系统 后端代码

---
+++ src/main/java/com/GraduationProject/chenzifeng/searchSystemOfLibraryBasedKnowledgeGraph/SearchSystemOfLibraryBasedKnowledgeGraphApplication.java
@@ -0,0 +1,13 @@
+package com.GraduationProject.chenzifeng.searchSystemOfLibraryBasedKnowledgeGraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchSystemOfLibraryBasedKnowledgeGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchSystemOfLibraryBasedKnowledgeGraphApplication.class, args);
+	}
+
+}
